284d5567f53ef4e1f45a4f8cc33df8e167cfc59d
Abhishek lohia <abhisheklohia@tripxl.com>
2026-05-29 12:28:35 +0530

    add changes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,14 @@
-import { RouterProvider } from 'react-router-dom';
-import { ConfigProvider, App as AntApp } from 'antd';
-import { router } from './router';
-export function App(){ return <ConfigProvider theme={{ token: { borderRadius: 12, colorPrimary: '#2563eb' } }}><AntApp><RouterProvider router={router}/></AntApp></ConfigProvider>; }
+import { RouterProvider } from "react-router-dom";
+import { ConfigProvider, App as AntApp } from "antd";
+import { router } from "./router";
+export function App() {
+  return (
+    <ConfigProvider
+      theme={{ token: { borderRadius: 12, colorPrimary: "#2563eb" } }}
+    >
+      <AntApp>
+        <RouterProvider router={router} />
+      </AntApp>
+    </ConfigProvider>
+  );
+}
